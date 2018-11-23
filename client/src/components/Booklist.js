270cefe2476989971d6a4a8fde3d6831b5911910
Clive Anthony Duncan <cad324@cornell.edu>
2018-11-23 17:53:26 -0500

    query results displayed on front-end

--- client/src/components/Booklist.js
+++ client/src/components/Booklist.js
@@ -7,17 +7,29 @@ const getBookQuery = gql`
     books {
       name
       genre
+      id
     }
   }
 `
 
 class BookList extends Component {
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return(<div>Loading books...</div>);
+    } else {
+      return data.books.map(book => {
+        return(
+          <li key={book.id}>{book.name}</li>
+        );
+      })
+    }
+  }
   render() {
-    console.log(this.props);
     return (
       <div>
         <ul id="book-list">
-          <li>Book name</li>
+          {this.displayBooks()}
         </ul>
       </div>
     );
